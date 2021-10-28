32bee7a40a917c0c2e06442b715641ae962a1f9b
Abderrahmane Tamazouzt <75092307+abderrahmane4dev@users.noreply.github.com>
2021-10-29 00:38:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let section = document.querySelector(".three");
 let spans = document.querySelectorAll(".progress span");
 window.onscroll = function() {
     if (window.scrollY > section.offsetTop) {
-        console.log("congrats");
+        
         spans.forEach((span) => {
             span.style.width = span.dataset.width;
         })
